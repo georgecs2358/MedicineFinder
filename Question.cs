a20d4ed2a6f5cc87ca88ad9c607d928c99fc7550
gm4321 <george.miller97@hotmail.co.uk>
2020-10-16 19:09:23 +0100

    Using 2d arrays

--- Question.cs
+++ Question.cs
@@ -7,27 +7,26 @@ namespace DrugFinder
     public Question()
     {
       ChildQuestionList = new List<Question>();
-      ResponseScores = new List<int>();
     }
 
     public List<Question> ChildQuestionList { get; set; }
     public string Title { get; set; }
-    public List<int> ResponseScores { get; set; }
+    public int[,] ResponseScores { get; set; }
     public int Response { get; set; }
 
     public void ExportScores()
     {
       if (Response == 0)
       {
-        Program.TotalDrugEffectScoreA += ResponseScores[0];
-        Program.TotalDrugEffectScoreB += ResponseScores[1];
-        Program.TotalDrugEffectScoreC += ResponseScores[2];
-        Program.TotalDrugEffectScoreD += ResponseScores[3];
+        Program.TotalDrugEffectScoreA += ResponseScores[0,0];
+        Program.TotalDrugEffectScoreB += ResponseScores[0,1];
+        Program.TotalDrugEffectScoreC += ResponseScores[0,2];
+        Program.TotalDrugEffectScoreD += ResponseScores[0,3];
       } else if (Response == 1) {
-        Program.TotalDrugEffectScoreA += ResponseScores[4];
-        Program.TotalDrugEffectScoreB += ResponseScores[5];
-        Program.TotalDrugEffectScoreC += ResponseScores[6];
-        Program.TotalDrugEffectScoreD += ResponseScores[7];        
+        Program.TotalDrugEffectScoreA += ResponseScores[1,0];
+        Program.TotalDrugEffectScoreB += ResponseScores[1,1];
+        Program.TotalDrugEffectScoreC += ResponseScores[1,2];
+        Program.TotalDrugEffectScoreD += ResponseScores[1,3];        
       }
     }
   }
--- QuestionTree.cs
+++ QuestionTree.cs
@@ -11,11 +11,14 @@ namespace DrugFinder
       Question questionC = new Question
       { 
         Title="Is the patient hostile to medicine?",
-        ResponseScores={3, 9, 5, 3, 9, 2, 4, 10},
+        ResponseScores = new int[2,4]{
+          {3, 9, 5, 3}, 
+          {9, 2, 4, 10}
+        },
 
         // Response=0 'YES, they are dubious about anything made in a lab.'
         // Response=1 would be 'NO, eager for treatment at all costs.'
-        Response=0
+        Response=1
       };
 
       Question questionB = new Question
@@ -26,23 +29,30 @@ namespace DrugFinder
         // clinical trials data to calculate scientifically. They link the  
         // patient question response to the drug effect variables
 
+        ResponseScores = new int[2,4]{
+          {5, 5, 6, 0}, 
+          {5, 5, 0, 8}, 
+        },
+
         // Response=0 would be 'Nausea/vomiting' which gives PFS=5, 
         // time to treatment failure=5, Liver damage=6, Nausea=0
         // Response=1 'Stomach pain' gives ProgFreeSurvival=5, TTTF=5, 
         // Liver damage=0, Nausea=8
-        ResponseScores={5, 5, 6, 0, 5, 5, 0, 8}, 
-        Response=0
+        Response=1
       };
       
       Question questionA = new Question
       { 
         ChildQuestionList = { questionB, questionC },
         Title="What is the patient's main aim from treatment?",
-        ResponseScores={2, 8, 7, 2, 8, 3, 8, 10},
+        ResponseScores = new int[2,4]{
+          {2, 8, 7, 2}, 
+          {8, 3, 8, 10},
+        },
 
         // Response=0 'Improve my quality of life'
         // Response=1 'Eradicate the disease, be dedicated to chemotherapy.'
-        Response=0
+        Response=1
       };
 
       Questions.Add(questionA);
